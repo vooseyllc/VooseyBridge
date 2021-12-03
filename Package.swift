5447a3f79de5b83df0feaa0a696b09db768b143d
Justin Means <justincmeans@gmail.com>
2021-12-03 12:39:23 -0700

    update package.swift

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
             targets: ["VooseyBridge"]),
     ],
     dependencies: [
-		.package(url: "https://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:x-oauth-basic@github.com/JustinMeans/JBS", .branch("main")),
+		.package(url: "https://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:x-oauth-basic@github.com/JustinMeans/JBS", .branch("main")),
 //		.package(name: "JBS", path: "/Users/JustinMeans/documents/JMLLC/JWS/JBS")
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),

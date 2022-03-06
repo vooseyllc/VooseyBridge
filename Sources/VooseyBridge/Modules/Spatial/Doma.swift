cfe0dd4dcd60705687a1dd79a723da51c9e74cea
Justin Means <justincmeans@gmail.com>
2022-03-05 18:08:22 -0700

    Adds enhanced emails with SwiftHTML

---
+++ Sources/VooseyBridge/Modules/Spatial/Doma.swift
@@ -0,0 +1,39 @@
+////
+////  File.swift
+////  
+////
+////  Created by Justin Means on 3/1/22.
+////
+//
+//import Foundation
+//import JBSModerate
+//
+//public struct Doma {
+//	public struct Global: Codable, Identifiable, Hashable {
+//		public var id: UUID? { micro.id }
+//		public var tour: [TourItem]
+//		public var micro: Micro
+//		public var business: Business.Micro
+//	}
+//	
+//	public struct Micro: Codable, Identifiable, Hashable {
+//		public var id: UUID?
+//		public var title: String
+//		public var description: String
+//		public var websiteURL: String?
+//		public var coordinates: String?
+//		public var imageURLs: [String]
+//		public var business: Business.Micro
+//		public var sqft: Float?
+//		public var beds: Int?
+//		public var baths: Float?
+//		public var floors: Float?
+//		public var garages: Int?
+//		public var length: Float
+//		public var depth: Float
+//		public var height: Float
+//		public var tags: [String]?
+//		public var modelURL: String
+//		public var
+//	}
+//}
